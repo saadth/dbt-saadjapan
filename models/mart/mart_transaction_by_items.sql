d6c821a116daa9d489a2ed9621d68760b80b65f6
saadsilver.intl@gmail.com <saadsilver.intl@gmail.com>
2025-06-17 18:22:33 +0000

    fixed issue

---
+++ models/mart/mart_transaction_by_items.sql
@@ -0,0 +1,27 @@
+with
+    joined_xe as (select * from {{ ref('int_join_xe_and_convert') }}),
+
+organized as (
+    select 
+    order_id,
+    date_modified,
+    date_created,
+    status,
+    sku,
+    quantity,
+    price,
+    price_after_discount,
+    subtotal,
+    total,
+    currency,
+    exchange_rate,
+    thb_price,
+    thb_price_after_discount,
+    thb_subtotal,
+    thb_total,
+    created_via,
+    payment_method
+    from joined_xe )
+
+select *
+from organized
