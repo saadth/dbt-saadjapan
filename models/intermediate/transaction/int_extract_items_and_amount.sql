5daad71228955441c87c6378e2e38d04a82cc912
saadsilver.intl@gmail.com <saadsilver.intl@gmail.com>
2025-06-18 14:09:15 +0000

    commit

--- models/intermediate/transaction/int_extract_items_and_amount.sql
+++ models/intermediate/transaction/int_extract_items_and_amount.sql
@@ -9,7 +9,7 @@ with
             o.status,
             o.date_modified,
             o.total,
-            json_value(item_json, '$.sku') as sku,
+            if(json_value(item_json, '$.sku')='',json_value(item_json, '$.name'),json_value(item_json, '$.sku')) as sku,
             safe_cast(json_value(item_json, '$.quantity') as int64) as quantity,
             safe_cast(json_value(item_json, '$.subtotal') as float64) as subtotal
         from orders o, unnest(o.items_details) as item_json
