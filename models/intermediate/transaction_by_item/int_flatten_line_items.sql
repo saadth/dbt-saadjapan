cec5a581c95d9b645fce3b8bd3a6ee84d40982a3
SAAD SAAD <saadsilver.intl@gmail.com>
2025-06-25 15:50:34 +0000

    commit

--- models/intermediate/transaction_by_item/int_flatten_line_items.sql
+++ models/intermediate/transaction_by_item/int_flatten_line_items.sql
@@ -4,6 +4,10 @@ with
     ),
 flatten as(
 select
+    o.order_id,
+    o.date_modified,
+    o.date_created,
+    o.status,
     o.pk,
     if(json_value(item_json, '$.sku')='',json_value(item_json, '$.name'),json_value(item_json, '$.sku')) as sku,
     safe_cast(json_value(item_json, '$.quantity') as int64) as quantity,
