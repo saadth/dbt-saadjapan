bea232eeed97cfab451196e37f739826a4ec963c
SAAD SAAD <saadsilver.intl@gmail.com>
2025-06-25 08:28:12 +0000

    created pk

---
+++ models/intermediate/int_pk_creation.sql
@@ -0,0 +1,13 @@
+with orders as(
+select *
+from {{ ref('stg_saad_woocommerce_api__raw_woocommerce_orders') }}),
+
+make_pk as(
+select
+*,
+concat(safe_cast(order_id as string),status) as pk
+from orders)
+
+select 
+*
+from make_pk
