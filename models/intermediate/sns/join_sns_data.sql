ca64418cf214a17f6d2e4ec727d418216b1971ee
SAAD SAAD <saadsilver.intl@gmail.com>
2025-07-12 02:39:27 +0000

    commit

---
+++ models/intermediate/sns/join_sns_data.sql
@@ -0,0 +1,23 @@
+With ig_total as (
+    select *
+    from {{ ref('stg_saad_sns__ig_saadsilver_intl_total_followers') }}
+
+),
+ig as (
+    select *
+    from {{ ref('stg_saad_sns__ig_saadsilver_intl') }}
+
+),
+line as (
+    select *
+    from {{ ref('stg_saad_sns__line_saadsilver_intl') }}
+
+),
+tiktok as (
+    select *
+    from {{ ref('stg_saad_sns__tiktok_saadsilver_th') }}
+
+)
+
+select *
+from tiktok
