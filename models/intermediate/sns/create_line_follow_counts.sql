b108a9eb63ff2101342bf73aa30453a37d086ff0
SAAD SAAD <saadsilver.intl@gmail.com>
2025-07-12 05:59:05 +0000

    line follower count

---
+++ models/intermediate/sns/create_line_follow_counts.sql
@@ -0,0 +1,34 @@
+with line as (
+
+    select *
+    from {{ ref('stg_saad_sns__line_saadsilver_intl') }}
+),
+
+table_1 as (
+select *
+, row_number () over (order by date asc) as index
+from line
+),
+
+table_2 as (
+    select 
+    date as prev_date,
+    total_followers as prev_total_followers,
+    total_reach as prev_total_reach,
+    index+1 as index
+from table_1
+),
+
+inner_join as (
+select
+date,
+total_followers,
+total_reach,
+total_followers - prev_total_followers as follower_count,
+total_reach - prev_total_reach as reach_count
+from table_1
+left join table_2 as prev using (index)
+)
+
+select *
+from inner_join
