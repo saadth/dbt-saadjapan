dedc8b1e6fb900fb0e1f9bc1f13c0739b076868d
SAAD SAAD <saadsilver.intl@gmail.com>
2026-07-15 06:23:03 +0000

    google ads

--- models/mart/Polling/mart_polling_gogle_offline_purchase.sql
+++ models/mart/Polling/mart_polling_gogle_offline_purchase.sql
@@ -1,41 +1,17 @@
 with
     transactions as (select * from {{ ref('int_polling_add_line_items_to_transaction') }}),
+
 organized as (
-    select
-    order_id,
-    date_created,
-    date_modified,
-    status,
-    currency,
-    customer_id,
-    billing_email as email,
-    shipping_first_name as first_name,
-    shipping_last_name as last_name,
-    shipping_address_1 as address_1, 
-    shipping_city as city,
-    shipping_state as state,
-    shipping_postcode as postcode,
-    shipping_country as country,
-    payment_method,
-    created_via,
-    cpos_source,
-    cpos_payment_status,
-    gender,
-    age,
-    reference,
-    purchase_reason,
-    extracted_exchange_rate as exchange_rate,
-    thb_total,
-    thb_discount_total,
-    thb_final_amount,
-    line_sku,
-    line_product_name,
-    line_quantity,
-    line_price,
-    line_subtotal,
-    line_total,
-    from transactions)
+select
+order_id,
+billing_email as email_address,
+date_created,
+currency,
+total as conversion_value,
+'Purchase' as conversion_name,
+from transactions
+where created_via != "checkout" and billing_email != "support@saadjapan.com"
+)
 
 select *
 from organized
-order by date_created desc
